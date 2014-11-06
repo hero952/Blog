bab04ab4b4eb4fa3e8b2b73d2708a7112069c944
hero952 <532580503@qq.com>
2014-11-06 17:28:55 +0800

    web

---
+++ scripts/ajax.js
@@ -0,0 +1,33 @@
+
+
+
+
+function regen() {
+	
+// home
+		
+prepareSlideshow();
+	
+// about
+	
+prepareInternalnav();
+	
+// photos
+	
+preparePlaceholder();
+	prepareGallery();
+	
+// live
+		
+stripeTables();
+	highlightRows();
+	displayAbbreviations();
+	
+// contact
+	
+focusLabels();
+	prepareForms();
+
+}
+
+
